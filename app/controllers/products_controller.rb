5df2c558efd09362012701f3ac39a92619f24876
Jack <jconnolly444@gmail.com>
2023-10-25 12:30:38 -0500

    test exercises

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,25 @@
 class ProductsController < ApplicationController
-  def all_products
+  def index
     @products = Product.all
-    render :index
+    render json: @products
   end
-  def one_product
-    @product = Product.find_by(id: params["id"])
-    render :show
+
+  def show
+    product = Product.find_by(id: params["id"])
+    render json: {
+      id: product.id,
+      name: product.name,
+      price: product.price,
+      image_url: product.image_url,
+      description: product.description,
+      created_at: product.created_at,
+      updated_at: product.updated_at,
+    }
   end
+  # def one_product
+  #   @product = Product.find_by(id: params["id"])
+  #   render :show
+  # end
   # def second_product
   #   @product = Product.second
   #   render :show
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/all_products" => "products#all_products"
-  get "/one_product/:id" => "products#one_product"
+  get "/products" => "products#index"
+  get "/products/:id" => "products#show"
   # get "/second_product", controller: "products", action: "second_product"
   # get "/third_product", controller: "products", action: "third_product"
 end
