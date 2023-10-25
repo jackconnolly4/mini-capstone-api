a562cc4d8dd2a190956dbc1c6871f25ce9f385c3
Jack <jconnolly444@gmail.com>
2023-10-25 14:31:48 -0500

    delete and patch tests

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
   post "/products" => "products#create"
-
+  patch "/products/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
   # get "/second_product", controller: "products", action: "second_product"
   # get "/third_product", controller: "products", action: "third_product"
 end
