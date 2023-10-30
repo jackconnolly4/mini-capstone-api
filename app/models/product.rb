810429ab5841c565b7b0d1df13274b834a5541a0
Jack <jconnolly444@gmail.com>
2023-10-30 13:35:06 -0500

    added validations and commented them out

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ApplicationRecord
+  # validates :name, presence: true
+  # validates :name, uniqueness: true
+  # validates :name, length: { in: 2..10 }
+  # validates :price, presence: true
+  # validates :price, numericality: { greater_than: 0 }
+  # validates :description, length: {in: 2..450}
+
+  def tax
+    price * 0.69
+  end
+  def total
+    price + tax
+  end
 end
--- app/views/products/_product.json.jbuilder
+++ app/views/products/_product.json.jbuilder
@@ -5,3 +5,4 @@ json.name product.name
 json.price product.price
 json.image_url product.image_url
 json.description product.description
+# json.inventory product.inventory
