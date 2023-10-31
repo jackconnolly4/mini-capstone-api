6362b60b13150477de9ad536045883b9fb8f2316
Jack <jconnolly444@gmail.com>
2023-10-31 12:23:36 -0500

    added supplier method to product.rb and products method to supplier.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,11 +5,16 @@ class Product < ApplicationRecord
   # validates :price, presence: true
   # validates :price, numericality: { greater_than: 0 }
   # validates :description, length: {in: 2..450}
-
+  def is_discounted?
+    price <= 10
+  end
   def tax
     price * 0.69
   end
   def total
     price + tax
+  end
+  def supplier
+    Supplier.find_by(id: supplier_id)
   end
 end
