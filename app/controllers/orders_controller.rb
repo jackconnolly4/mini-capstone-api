f4eb93f46584d1ae88c3d7a81e2af351c79ab988
Jack <jconnolly444@gmail.com>
2023-11-15 14:25:58 -0600

    subtotal, tax and total calculations working now

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,10 @@
 class OrdersController < ApplicationController
   def create
-    product = Product.find(params["product_id"])
-    price = product.price
-    subtotal = (price * params["quantity"].to_i)
-    @order = Order.create(user_id: current_user.id, product_id: params["product_id"], quantity: params["quantity"], tax: (subtotal * 0.09), total: (subtotal + price))
+    product = Product.find_by(id: params["product_id"])
+    calculated_subtotal = product.price * params["quantity"].to_i
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+    @order = Order.create(user_id: current_user.id, product_id: params["product_id"],quantity: params["quantity"], subtotal: calculated_subtotal, tax: calculated_tax, total: calculated_total)
     if @order.valid?
       render :show
       else  
