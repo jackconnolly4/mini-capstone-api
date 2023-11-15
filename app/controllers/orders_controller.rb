22fdb7116f2e01325fd6ee223d974a2685935a89
Jack <jconnolly444@gmail.com>
2023-11-15 12:40:30 -0600

    added order show and index actions

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,4 +7,12 @@ class OrdersController < ApplicationController
         render json: { errors: @order.errors.full_messages }, status: :unprocessable_entity
       end
   end
+  def show
+    @order = Order.find_by(id: params["id"])
+    render :show
+  end
+  def index
+    @orders = Order.all
+    render :index
+  end
 end
