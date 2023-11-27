a59c36d34b6f75a1c4fc4d8cc720b8abeeee39c3
Jack <jconnolly444@gmail.com>
2023-11-27 12:39:36 -0600

    added has_many through relationships

--- app/models/product.rb
+++ app/models/product.rb
@@ -27,4 +27,12 @@ class Product < ApplicationRecord
   has_many :orders
 
   has_many :category_products
+
+
+  has_many :categories, through: :category_products
+  # def categories
+  #   category_products.map do |category_product|
+  #     category_product.category
+  #   end
+  # end
 end
